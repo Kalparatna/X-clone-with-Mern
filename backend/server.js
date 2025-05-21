f58b427b2b0d54b76eb4b6da16362bc011a0b010
Kalparatna Mahajan <125982626+Kalparatna@users.noreply.github.com>
2025-05-21 17:08:03 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ const app = express()
 connectDB()
 
 app.use(cors({
-  origin: ['http://localhost:5173', 'https://x-clone-with-mern.vercel.app'],
+  origin: ['*'],
   credentials: true
 }))
 app.use(express.json())
