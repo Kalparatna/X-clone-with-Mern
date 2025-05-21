cef33238fa782036d3eb487ff0c8f80f0e6feeff
Kalparatna Mahajan <125982626+Kalparatna@users.noreply.github.com>
2025-05-21 17:44:12 +0530

    Update protectRoute.js

--- backend/middleware/protectRoute.js
+++ backend/middleware/protectRoute.js
@@ -14,7 +14,8 @@ export const protectRoute = async (req, res, next) => {
             return res.status(401).json({ error: "Unauthorized: Invalid token" });
         }
         
-        const user = await User.findById(decoded.uid).select("-password");
+        const user = await User.findById(decoded.id).select("-password");
+
 
         if (!user) {
             return res.status(404).json({ error: "User not found" });
